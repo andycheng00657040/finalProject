ae4622bd89ffd5f216206b26ab8811eb96f9bf51
saltychess <37352173@qq.com>
2020-06-24 14:57:16 +0800

    Initial Commit

---
+++ finalProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  finalProject
+//
+//  Created by User08 on 2020/6/24.
+//  Copyright © 2020 00657040. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
